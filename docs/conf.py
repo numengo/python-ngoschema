4752679190fbc7934fabc2646dcba656b4ba38af
Cedric ROMAN <roman@numengo.com>
2022-03-23 07:40:27 +0100

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = 'NgoSchema'
 year = '2018'
 author = 'Cédric ROMAN'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.0.4'
+version = release = '1.0.5'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- ngoschema/__init__.py
+++ ngoschema/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Cedric ROMAN"""
 __email__ = "roman@numengo.com"
-__version__ = "__version__ = '1.0.4'"
+__version__ = "__version__ = '1.0.5'"
 
 # load settings
 from simple_settings import LazySettings
