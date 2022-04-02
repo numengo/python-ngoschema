e398e0ed636c0440fd12b47065e47bc7b91742f1
Cedric ROMAN <roman@numengo.com>
2022-04-02 20:42:26 +0200

    Bump version: 1.0.7 → 1.0.8

--- ngoschema/__init__.py
+++ ngoschema/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Cedric ROMAN"""
 __email__ = "roman@numengo.com"
-__version__ = "__version__ = '1.0.7'"
+__version__ = "__version__ = '1.0.8'"
 
 # load settings
 from simple_settings import LazySettings
